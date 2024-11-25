b559e36db1a55c04e9dc7a917bba4688aa5cae93
bunny646 <snbhk425@gmail.com>
2024-11-25 16:31:43 +0530

    package.sh

--- package.sh
+++ package.sh
@@ -6,3 +6,11 @@ echo "please enter your username"
 read USERNAME
 
 echo "username entered is : $USERNAME"
+
+echo "please enter your password"
+
+read PASSWORD
+
+echo "password enterd is : $PASSWORD"
+
+
